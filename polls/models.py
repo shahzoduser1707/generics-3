4d1a0d59d5971c36a3617c30b7ae9d017ac77f9b
Shahzod <tohirovshahzod1707@gmail.com>
2023-09-18 01:57:55 +0500

    only get permission added

--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import datetime
+from django.contrib.auth.models import User
 # Create your models here.
 
 class LaptopModel(models.Model):
@@ -8,6 +9,7 @@ class LaptopModel(models.Model):
     function = models.CharField(max_length=200,default='')
     price = models.IntegerField(default=0)
     desc = models.TextField(default='write smth pls!')
+    user = models.ForeignKey(User,on_delete=models.CASCADE,default=1)
 
     class Meta:
         db_table = 'LaptopModel'
--- polls/views.py
+++ polls/views.py
@@ -3,12 +3,14 @@ from rest_framework.response import Response
 from .models import LaptopModel
 from .serializers import LaptopSerializer
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 
 class GetLaptop(generics.ListAPIView):
     queryset = LaptopModel.objects.all()
     serializer_class = LaptopSerializer
+    permission_classes = (IsAuthenticated,)
 
 class CreateLaptop(generics.CreateAPIView):
     queryset = LaptopModel.objects.all()
